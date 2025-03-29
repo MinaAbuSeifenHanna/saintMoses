4c1d2c8566d099c717adec370682de1223aa8fdf
Mina AbuSeifen <152615124+MinaAbuSeifenHanna@users.noreply.github.com>
2025-03-29 22:28:13 +0200

    Deploy to GitHub Pages

---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_ar.dart
@@ -0,0 +1,29 @@
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get service_title => 'خدمة القديس موسى الأسود';
+
+  @override
+  String get toggle_theme => 'تبديل الوضع';
+
+  @override
+  String get toggle_language => 'تغيير اللغة';
+
+  @override
+  String get service_name => 'القديس موسى الأسود';
+
+  @override
+  String get quote => 'لا تمنع الخير عن اهلة حين يكون في طاقة يدك ان تفعلة. امثال ٣٧:٣';
+
+  @override
+  String get search_family => 'ابحث عن الأسرة بالرقم القومي';
+
+  @override
+  String get add_family => 'إضافة أسرة';
+}
---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_en.dart
@@ -0,0 +1,29 @@
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get service_title => 'Saint Moses the Black Service';
+
+  @override
+  String get toggle_theme => 'Toggle Theme';
+
+  @override
+  String get toggle_language => 'Toggle Language';
+
+  @override
+  String get service_name => 'Saint Moses the Black';
+
+  @override
+  String get quote => 'Do not withhold good from those to whom it is due when it is in the power of your hand to do so. Proverbs 3:37';
+
+  @override
+  String get search_family => 'Search by National ID';
+
+  @override
+  String get add_family => 'Add Family';
+}
